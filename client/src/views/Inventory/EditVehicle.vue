4d36f4a3e82e8d412f913171339807cfdb577f17
Matthew Beckwith <mbeckwith@morganautogroup.com>
2026-08-17 09:34:18 -0400

    fixing app context URL

--- client/src/views/Inventory/EditVehicle.vue
+++ client/src/views/Inventory/EditVehicle.vue
@@ -75,7 +75,7 @@ import { onMounted, ref, computed, type Ref, type ComputedRef } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 import { InventoryItem } from '../../utilities/models/inventory.ts'
-import { Columns, DEV_URL } from '../../utilities/constants/inventory.ts'
+import { Columns, BASE_URL } from '../../utilities/constants/inventory.ts'
 import type { Attachment, Item, RawFiles } from '../../utilities/types/inventory.ts'
 
 import Navigation from '../../components/global/Navigation.vue'
@@ -158,17 +158,15 @@ const refMapping: Record<string, Ref<any>> = {
   INVENTORY_NOTES: inventoryNotes
 }
 
-const BASE_URL: Ref<String | null> = ref(null)
+const url: Ref<String | null> = ref(null)
 
 onMounted(async () => {
   const contextRes = await monday.get("context");
   const context = (contextRes as any)?.data;
 
-  BASE_URL.value = context?.appVersion?.mondayCodeHostingUrl
-    ? `${context.appVersion.mondayCodeHostingUrl}/api/`
-    : DEV_URL; // fallback for dev
+  url.value = BASE_URL; // fallback for dev
   try {
-    const response = await fetch(`${BASE_URL.value}inventory/edit-vehicle/${route.params.itemId}`)
+    const response = await fetch(`${url.value}inventory/edit-vehicle/${route.params.itemId}`)
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`)
     }
@@ -213,7 +211,7 @@ const attachmentList: ComputedRef<Attachment[]> = computed(() => {
 
       return parsed.files.map(file => {
         return {
-          url: String(`${BASE_URL.value}assets/${file.assetId}`),
+          url: String(`${url.value}assets/${file.assetId}`),
           name: String(file.name)
         }
       })
@@ -253,7 +251,7 @@ async function handleUpdate() {
   }
 
   try {
-    const response = await fetch(`${BASE_URL.value}inventory/update/${UID.value}`, {
+    const response = await fetch(`${url.value}inventory/update/${UID.value}`, {
       method: 'POST',
       body: formData,
     })
